1f461f54d862859606c7bc02009fc35a66d92574
Liang <15517072173@163.com>
2020-03-14 19:01:00 +0800

    first commit

---
+++ src/routes/IndexPage.tsx
@@ -0,0 +1,10 @@
+import React, {} from 'react';
+import styles from './IndexPage.less';
+
+const IndexPage: React.FC = () => {
+  return (
+    <h1 className={styles.h1}>Hello TypeScript</h1>
+  )
+};
+
+export default IndexPage;
